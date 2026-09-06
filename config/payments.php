db07620a9ac6d51f0fd3a1eb220ab9fa36f0b4cd
almostonehundredpercent <zyroncanchela420@gmail.com>
2026-09-06 19:43:42 +0800

    fix: support existing GCash recipient setting

--- config/payments.php
+++ config/payments.php
@@ -3,7 +3,7 @@
 return [
     'deposit_percentage' => (float) env('BOOKING_DEPOSIT_PERCENTAGE', 30),
     'minimum_deposit' => (float) env('BOOKING_MINIMUM_DEPOSIT', 500),
-    'gcash_recipient_name' => env('GCASH_RECIPIENT_NAME'),
+    'gcash_recipient_name' => env('GCASH_RECIPIENT_NAME', env('GCASH_RECIPTIENT_NAME')),
     'gcash_number' => env('GCASH_NUMBER'),
     'gcash_qr_url' => env('GCASH_QR_URL'),
 ];
