4ad42d017907cfb52c4ff999bec3baad405cc065
almostonehundredpercent <zyroncanchela420@gmail.com>
2026-09-11 11:06:56 +0800

    fix: keep production redirects on HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
+            if (! $this->app->runningInConsole()) {
+                URL::forceRootUrl('https://' . request()->getHost());
+            }
         }
     }
 }
