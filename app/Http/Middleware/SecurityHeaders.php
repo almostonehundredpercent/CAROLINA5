99ce50628cbf5f2db230f20ef9182486f4cb8b44
almostonehundredpercent <zyroncanchela420@gmail.com>
2026-09-12 10:53:34 +0800

    refactor: simplify security header middleware

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -4,14 +4,11 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
 
 class SecurityHeaders
 {
     public function handle(Request $request, Closure $next)
     {
-        $nonce = base64_encode(random_bytes(18));
-        View::share('cspNonce', $nonce);
         $response = $next($request);
         $response->headers->set('Content-Security-Policy', "default-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; object-src 'none'; img-src 'self' https://images.unsplash.com data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; script-src 'self' 'unsafe-inline'; connect-src 'self'; upgrade-insecure-requests");
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
